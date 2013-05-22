4ed963ea2a6cb36ad7cf589f341878a18170235b
Nero Sule <sulenero@gmail.com>
2013-05-22 21:30:46 +0300

    Initial commit

---
+++ MediaLibrary/MediaItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibrary
+{
+    public class MediaItem
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Album { get; set; }
+        public int Length { get; set; }
+        public string Location { get; set; }
+    }
+}
